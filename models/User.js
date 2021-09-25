5e5110a6e10bdadd3ea5f1e09cb6d8205a630953
aungkyawkhaing <agmsx.938@gmail.com>
2021-09-25 13:50:39 +0630

    Generate hash password in Schema

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {isEmail} =require('Validator');
+const bcrypt = require('bcrypt');
 
 const userSchema =new mongoose.Schema({
     email :{
@@ -15,6 +16,20 @@ const userSchema =new mongoose.Schema({
         minlength:[6,"Minimum password Length is 6"]
     }
 });
+userSchema.post('save',function(data,next){
+    next();
+});
+
+userSchema.pre('save',function(next){
+    const saltRounds=8;
+    bcrypt.genSalt(saltRounds,(err,salt)=>{
+        bcrypt.hash(this.password,salt,(error,hash)=>{
+            this.password = hash;
+            console.log("user about to create this",this);
+            next();
+        })
+    })
+})
 
 const User = mongoose.model('user',userSchema);
 module.exports = User;
